c59d9e3473a2f28a9a3512d974cfc82d84ceda85
Sam Thomas <sxt643@student.bham.ac.uk>
2022-12-20 14:58:52 +0100

    Cmake Structure Complete

---
+++ 01/AoC011222.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "1st December Hello, World!" << std::endl;
+    return 0;
+}
---
+++ 02/AoC021222.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "2nd December Hello, World!" << std::endl;
+    return 0;
+}
