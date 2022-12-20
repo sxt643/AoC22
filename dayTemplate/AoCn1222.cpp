b5ddf1e136e6b4341b33189212aa9e31835fa21d
Sam Thomas <sxt643@student.bham.ac.uk>
2022-12-20 18:07:30 +0100

    Daily Template

---
+++ dayTemplate/AoCn1222.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int testFunc(const string &filename) {
+    int sum = 0;
+
+    ifstream fileRead(filename);
+    string lineText;
+
+    while(getline(fileRead, lineText)) {
+
+    }
+
+    fileRead.close();
+
+    return sum;
+}
+
+int main() {
+    const string filename = "";
+    cout << "nth December" << endl;
+
+//    cout << "Silver task\t-\tScore according to original strategy: " << scoreOrigStrategy(filename) << std::endl;
+//    cout << "Gold task\t-\tScore according to new strategy: " << scoreNewStrategy(filename) << std::endl;
+
+    return 0;
+}
